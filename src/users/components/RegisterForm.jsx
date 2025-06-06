8227444b38ae4150d0775b46f7a7f47439b9a143
Dsaar <danieljoshuasaar@gmail.com>
2025-06-06 15:43:39 +0300

    added a hook called useForm to registerForm

--- src/users/components/RegisterForm.jsx
+++ src/users/components/RegisterForm.jsx
@@ -1,70 +1,55 @@
 import { Button, Container, TextField } from '@mui/material'
 import React, { useState } from 'react'
 import Joi from 'joi'
+import useForm from '../../hooks/useForm';
 
 function RegisterForm() {
-	const [userDetails, setUserDetails] = useState(
+	const { formDetails, errors, handleChange, handleSubmit } = useForm(
+		{ firstName: "", lastName: "", middleName: "" },
 		{
-			firstName: '',
-			lastName: '',
-			middleName: ''
-
+			firstName: Joi.string().min(2).max(10),
+			middleName: Joi.string().allow(""),
+			lastName: Joi.string().min(2).required(),
 		}
 	);
 
-	const [errors, setErrors] = useState({});
-
-	const schemeObj = {
-		firstName: Joi.string().min(2).max(10),
-		middleName: Joi.string().optional().allow(''),
-		lastName: Joi.string().min(2).required()
-	}
-	const scheme = Joi.object(schemeObj)
-
-	const handleSignup = () => {
-		console.log(userDetails)
-
-		const { error } = scheme.validate(userDetails, { abortEarly: false })
-
-		console.log(error);
-
-	}
-
-	const handleChange = (event) => {
-		const fieldName = event.target.name;
-		const fieldValue = event.target.value;
-
-		setUserDetails((prev) => ({
-			...prev,
-			[fieldName]: fieldValue,
-		}));
-
-
-		const fieldScheme = Joi.object({ [fieldName]: schemeObj[fieldName] });
-
-		const { error } = fieldScheme.validate({ [fieldName]: fieldValue });
-		if (error) {
-			setErrors({ [fieldName]: error.details[0].message });
-
-		} else {
-			setErrors((prev) => {
-				delete prev[fieldName];
-				return prev;
-			})
-		}
-	};
 	return (
-		<Container sx={{ pt: 2 }}>
-			<TextField label={'First Name'}
-				name='firstName' onChange={handleChange} error={Boolean(errors.firstName)}
-				helperText={errors.firstName} />
-			<TextField label={'Middle Name'} name='middleName' onChange={handleChange} error={Boolean(errors.middleName)} helperText={errors.middleName} />
-			<TextField label={'Last Name'} name='lastName' onChange={handleChange} error={Boolean(errors.lastName)} helperText={errors.lastName} />
+		<Container
+			sx={{
+				pt: 2,
+			}}
+		>
+			<TextField
+				label={"first name"}
+				name="firstName"
+				onChange={handleChange}
+				error={Boolean(errors.firstName)}
+				helperText={errors.firstName}
+			/>
+			<TextField
+				label={"middle name"}
+				name="middleName"
+				onChange={handleChange}
+				error={Boolean(errors.middleName)}
+				helperText={errors.middleName}
+			/>
+			<TextField
+				label={"last name"}
+				name="lastName"
+				onChange={handleChange}
+				error={Boolean(errors.lastName)}
+				helperText={errors.lastName}
+			/>
 
-			<Button variant='contained' sx={{ display: 'block' }} onClick={handleSignup}>Sign Up</Button>
+			<Button
+				variant="contained"
+				sx={{ display: "block" }}
+				onClick={handleSubmit}
+			>
+				Sign up
+			</Button>
 		</Container>
-
-	)
+	);
 }
 
-export default RegisterForm
+export default RegisterForm;
