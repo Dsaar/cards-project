31b4cbebd81c0e4ee479ff0e8105ab7ef5d925d9
Dsaar <danieljoshuasaar@gmail.com>
2025-06-09 21:49:46 +0300

    added new sections to register form

--- src/users/components/RegisterForm.jsx
+++ src/users/components/RegisterForm.jsx
@@ -1,54 +1,139 @@
-import { Button, Container, TextField } from '@mui/material'
-import Joi from 'joi'
-import useForm from '../../hooks/useForm';
+import {
+	Button,
+	Checkbox,
+	Container,
+	FormControlLabel,
+	Grid,
+	Paper,
+	TextField,
+	Typography,
+} from "@mui/material";
+import Joi from "joi";
+import useForm from "../../hooks/useForm";
 
 function RegisterForm() {
 	const { formDetails, errors, handleChange, handleSubmit } = useForm(
-		{ firstName: "", lastName: "", middleName: "" },
 		{
-			firstName: Joi.string().min(2).max(10),
+			firstName: "",
+			middleName: "",
+			lastName: "",
+			email: "",
+			phone: "",
+			password: "",
+			imageUrl: "",
+			imageAlt: "",
+			state: "",
+			country: "",
+			city: "",
+			street: "",
+			houseNumber: "",
+			zip: "",
+			isBusiness: false,
+		},
+		{
+			firstName: Joi.string().min(2).max(10).required(),
 			middleName: Joi.string().allow(""),
 			lastName: Joi.string().min(2).required(),
+			email: Joi.string().email({ tlds: false }).required(),
+			phone: Joi.string().required(),
+			password: Joi.string().min(6).required(),
+			imageUrl: Joi.string().uri().allow(""),
+			imageAlt: Joi.string().allow(""),
+			state: Joi.string().allow(""),
+			country: Joi.string().required(),
+			city: Joi.string().required(),
+			street: Joi.string().required(),
+			houseNumber: Joi.string().required(),
+			zip: Joi.string().allow(""),
+			isBusiness: Joi.boolean(),
 		}
 	);
 
 	return (
-		<Container
-			sx={{
-				pt: 2,
-			}}
-		>
-			<TextField
-				label={"first name"}
-				name="firstName"
-				onChange={handleChange}
-				error={Boolean(errors.firstName)}
-				helperText={errors.firstName}
-			/>
-			<TextField
-				label={"middle name"}
-				name="middleName"
-				onChange={handleChange}
-				error={Boolean(errors.middleName)}
-				helperText={errors.middleName}
-			/>
-			<TextField
-				label={"last name"}
-				name="lastName"
-				onChange={handleChange}
-				error={Boolean(errors.lastName)}
-				helperText={errors.lastName}
-			/>
+		<Container maxWidth="md" sx={{ py: 5 }}>
+			<Paper elevation={3} sx={{ p: 4, bgcolor: "#eaf4fc" }}>
+				<Typography variant="h5" align="center" gutterBottom>
+					REGISTER FORM
+				</Typography>
 
-			<Button
-				variant="contained"
-				sx={{ display: "block" }}
-				onClick={handleSubmit}
-			>
-				Sign up
-			</Button>
+				<Grid container spacing={2} sx={{display:'flex', justifyContent:'center'}}>
+					{[
+						{ label: "First name *", name: "firstName" },
+						{ label: "Middle name", name: "middleName" },
+						{ label: "Last name *", name: "lastName" },
+						{ label: "Phone *", name: "phone" },
+						{ label: "Email *", name: "email" },
+						{ label: "Password *", name: "password", type: "password" },
+						{ label: "Image url", name: "imageUrl" },
+						{ label: "Image alt", name: "imageAlt" },
+						{ label: "State", name: "state" },
+						{ label: "Country *", name: "country" },
+						{ label: "City *", name: "city" },
+						{ label: "Street *", name: "street" },
+						{ label: "House number *", name: "houseNumber" },
+						{ label: "Zip", name: "zip" },
+					].map(({ label, name, type = "text" }) => (
+						<Grid item xs={12} sm={6} key={name}>
+							<TextField
+								fullWidth
+								label={label}
+								name={name}
+								type={type}
+								value={formDetails[name]}
+								onChange={handleChange}
+								error={Boolean(errors[name])}
+								helperText={errors[name]}
+								sx={{minWidth:'300px'}}
+							/>
+						</Grid>
+					))}
+
+					<Grid item xs={12} sx={{minWidth:'700px', display:'flex', justifyContent:'center'}}>
+						<FormControlLabel
+							control={
+								<Checkbox
+									name="isBusiness"
+									checked={formDetails.isBusiness}
+									onChange={(e) =>
+										handleChange({
+											target: {
+												name: "isBusiness",
+												value: e.target.checked,
+											},
+										})
+									}
+								/>
+							}
+							label="Sign up as business"
+						/>
+					</Grid>
+
+					<Grid item xs={12} sm={6} sx={{minWidth:'200px'}}>
+						<Button
+							fullWidth
+							variant="outlined"
+							color="error"
+							onClick={() => console.log("Canceled")}
+						>
+							Cancel
+						</Button>
+					</Grid>
+
+					<Grid item xs={12} sm={6} sx={{minWidth:'200px'}}>
+						<Button
+							fullWidth
+							variant="contained"
+							onClick={handleSubmit}
+							disabled={Object.keys(errors).length > 0}
+						>
+							Submit
+						</Button>
+					</Grid>
+				</Grid>
+			</Paper>
 		</Container>
 	);
 }
 
 export default RegisterForm;
+  
