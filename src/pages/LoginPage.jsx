1434971e83f5f0d4e7f5bdf74fc29046e55c585f
Dsaar <danieljoshuasaar@gmail.com>
2025-06-24 14:49:00 +0300

    login form style

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import LoginForm from '../users/components/loginForm'
+import LoginForm from '../users/components/LoginForm'
+import { Box } from '@mui/material'
 
 function LoginPage() {
   return (
-    <div>
+    <Box
+      sx={{
+        minHeight: '100vh',
+        backgroundColor: '#e3f2fd', // light blue
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        p: 0,
+        m:-7
+      }}
+    >
       <LoginForm />
-    </div>
+    </Box>
   )
 }
 
--- src/users/components/LoginForm.jsx
+++ src/users/components/LoginForm.jsx
@@ -1,35 +1,37 @@
-import { TextField } from "@mui/material";
-import Form from "../../components/Form";
-import useForm from "../../hooks/useForm";
+import {
+	TextField,
+	Grid,
+	Box,
+	Button,
+} from "@mui/material";
+import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
+import LoopIcon from "@mui/icons-material/Loop";
+import { useNavigate, Navigate } from "react-router-dom";
 import axios from "axios";
-import { use } from "react";
+import useForm from "../../hooks/useForm";
 import loginSchema from "../models/loginSchema";
 import initialLoginForm from "../helpers/initialForms/initialLogInForm";
 import { getUser, setTokenInLocalStorage } from "../services/localStorageService";
 import { useCurrentUser } from "../providers/UserProvider";
-import { Navigate } from 'react-router-dom'
-
-
-
+import Form from "../../components/Form";
 
 function LoginForm() {
-	const { user } = useCurrentUser()
+	const { user, setUser, setToken } = useCurrentUser();
+	const navigate = useNavigate();
 
-	if (user) {
-		return <Navigate to={'/'} replace />
-	}
+	if (user) return <Navigate to="/" replace />;
 
-	const {setToken,setUser}=useCurrentUser()
-
-	const handleLogin = async (user) => {
+	const handleLogin = async (credentials) => {
 		try {
-			const response = await axios.post('https://monkfish-app-z9uza.ondigitalocean.app/bcard2/users/login', user);
-			console.log(response);
+			const response = await axios.post(
+				"https://monkfish-app-z9uza.ondigitalocean.app/bcard2/users/login",
+				credentials
+			);
 			setTokenInLocalStorage(response.data);
 			setToken(response.data);
 			setUser(getUser());
 		} catch (error) {
-			alert('The login has failed')
+			alert("Login failed");
 		}
 	};
 
@@ -43,24 +45,50 @@ function LoginForm() {
 		<Form
 			onSubmit={handleSubmit}
 			onReset={() => { }}
-			title={"sign in form"}
-			styles={{ maxWidth: "800px" }}
+			title="Sign In Form"
+			styles={{ maxWidth: "900px" }}
 		>
-			<TextField
-				name="email"
-				label="email"
-				error={errors.email}
-				onChange={handleChange}
-				value={formDetails.email}
-			/>
-			<TextField
-				name="password"
-				label="password"
-				error={errors.password}
-				onChange={handleChange}
-				value={formDetails.password}
-				type="password"
-			/>
+			<Grid container spacing={2} sx={{ display: "flex", justifyContent: "center" }}>
+				<Grid item xs={12} sm={6}>
+					<TextField
+						fullWidth
+						sx={{ width: "400px" }}
+						name="email"
+						label="Email"
+						type="email"
+						value={formDetails.email}
+						onChange={handleChange}
+						error={!!errors.email}
+						helperText={errors.email}
+					/>
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<TextField
+						fullWidth
+						sx={{ width: "400px" }}
+						name="password"
+						label="Password"
+						type="password"
+						value={formDetails.password}
+						onChange={handleChange}
+						error={!!errors.password}
+						helperText={errors.password}
+					/>
+				</Grid>
+
+				<Grid item xs={12}>
+					<Box sx={{ width: "820px", mx: "auto" }}>
+						<Button
+							fullWidth
+							variant="outlined"
+							startIcon={<PersonAddAltIcon />}
+							onClick={() => navigate("/register")}
+						>
+							Sign Up
+						</Button>
+					</Box>
+				</Grid>
+			</Grid>
 		</Form>
 	);
 }
