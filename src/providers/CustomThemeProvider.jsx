fce61c920ccb123c51e7ae66c87759ff7b9269ca
Dsaar <danieljoshuasaar@gmail.com>
2025-06-24 15:07:37 +0300

    adding global reset to project

--- src/providers/CustomThemeProvider.jsx
+++ src/providers/CustomThemeProvider.jsx
@@ -1,45 +1,52 @@
 import { createContext, useContext, useState } from "react";
-import { createTheme, ThemeProvider } from "@mui/material";
+import {
+	createTheme,
+	ThemeProvider,
+	CssBaseline,
+	GlobalStyles,
+} from "@mui/material";
 
-
-//step 1: create the context
-
+// Step 1: create the context
 const ThemeContext = createContext();
 
-//step 2: create the provider
-
-export default function customThemeProvider({ children }) {
-	const [isDark, setIsDark] = useState(false)
+// Step 2: create the provider
+export default function CustomThemeProvider({ children }) {
+	const [isDark, setIsDark] = useState(false);
 
 	const toggleMode = () => {
-		setIsDark(prev => !prev)
-	}
+		setIsDark((prev) => !prev);
+	};
 
 	const theme = createTheme({
 		palette: {
-			mode: isDark ? 'dark' : 'light'
-		}
-	})
+			mode: isDark ? "dark" : "light",
+		},
+	});
 
-	return <ThemeProvider theme={theme}>
-		<ThemeContext.Provider value={{toggleMode,isDark}}>{children}</ThemeContext.Provider>
-	</ThemeProvider>
-
-
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<GlobalStyles
+				styles={{
+					"*": {
+						margin: 0,
+						padding: 0,
+						boxSizing: "border-box",
+					},
+				}}
+			/>
+			<ThemeContext.Provider value={{ toggleMode, isDark }}>
+				{children}
+			</ThemeContext.Provider>
+		</ThemeProvider>
+	);
 }
 
-//step 3: create custom hook for using the context (optional)
-
+// Step 3: create custom hook for using the context (optional)
 export const useTheme = () => {
-
 	const context = useContext(ThemeContext);
-
 	if (!context) {
-
 		throw new Error("useTheme must be used within a provider");
-
 	}
-
 	return context;
-
 };
