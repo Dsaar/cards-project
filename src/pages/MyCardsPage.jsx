5b8cd0b723367287c653ea5d666a57a55f10275a
Dsaar <danieljoshuasaar@gmail.com>
2025-06-27 15:37:14 +0300

    add myCards display logic

--- src/pages/MyCardsPage.jsx
+++ src/pages/MyCardsPage.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Container, Typography } from "@mui/material";
+import BCards from "../cards/components/Bcards";
+import { getToken } from "../users/services/localStorageService";
 
 function MyCardsPage() {
+  const [myCards, setMyCards] = useState([]);
+
+  useEffect(() => {
+    const fetchMyCards = async () => {
+      try {
+        const token = getToken();
+        const response = await axios.get(
+         "https://monkfish-app-z9uza.ondigitalocean.app/bcard2/cards/my-cards",
+          {
+            headers: {
+              "x-auth-token": token,
+            },
+          }
+        );
+        setMyCards(response.data);
+      } catch (error) {
+        console.error("Failed to fetch your cards:", error);
+      }
+    };
+
+    fetchMyCards();
+  }, []);
+
   return (
-	<div>MyCardsPage</div>
-  )
+    <Container sx={{ paddingBottom: 10 }}>
+      <Typography variant="h4" gutterBottom>My Cards</Typography>
+      <BCards cards={myCards} />
+    </Container>
+  );
 }
 
-export default MyCardsPage
+export default MyCardsPage;
